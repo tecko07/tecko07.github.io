ad59fac143ce4d2d4fd7b4da5465a1c2dbf8fb7e
tecko07 <56227647+tecko07@users.noreply.github.com>
2019-12-02 13:14:49 +0200

    Add files via upload

--- svg/svg.js
+++ svg/svg.js
@@ -1,4 +1,4 @@
-document.getElementById('id_logic_version').innerHTML = "Logic = 2019.12.02.0";
+document.getElementById('id_logic_version').innerHTML = "Logic = 2019.12.02.1";
 
 window.addEventListener("deviceorientation", on_gyro_data_uab);
 window.addEventListener("devicemotion", on_acc_data_uab);
@@ -12,6 +12,7 @@ function deseneaza(unghi_x, unghi_y) {
 	//construim o cale
 	context.beginPath();
 
+	var r = circle.getAttribute("r");
 	//adaugam un cerc la cale
 	var x = unghi_x / 90 * (svg.width / 2 - r) + svg.width / 2; 
 	var y = unghi_y / 90 * (svg.height / 2 - r) + svg.height / 2; 
