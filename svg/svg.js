429ad91576e80a7e6f6744887ae699e51b3c1264
tecko07 <56227647+tecko07@users.noreply.github.com>
2019-12-02 13:23:11 +0200

    Add files via upload

--- svg/svg.js
+++ svg/svg.js
@@ -1,4 +1,4 @@
-document.getElementById('id_logic_version').innerHTML = "Logic = 2019.12.02.1";
+document.getElementById('id_logic_version').innerHTML = "Logic = 2019.12.02.2";
 
 window.addEventListener("deviceorientation", on_gyro_data_uab);
 window.addEventListener("devicemotion", on_acc_data_uab);
