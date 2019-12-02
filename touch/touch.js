7f92f1f53c5ac80e3495801e5c16fddee0613924
tecko07 <56227647+tecko07@users.noreply.github.com>
2019-12-02 11:09:35 +0200

    Add files via upload

---
+++ touch/touch.js
@@ -0,0 +1,20 @@
+doument.getElementById("id_logic").innerHTML= "Logic: 2019.12.02.0";
+
+window.addEventListener("touchstart", touch_start_uab);
+
+var canvas = document.getElementById("id_canvas");
+var context = canvas.getContext("2d");
+
+function touch_start_uab(p)
+{
+	var t = p.changeTouches;
+	for (var i = 0; i < t.length; i++)
+	{
+		//desenam un cerc
+		context.beginPath();
+		context.arc(t[i].pageX, t[i].pageY, 10, 0, 2 * Math.PI);
+		context.stroke();
+
+		
+	}
+}
